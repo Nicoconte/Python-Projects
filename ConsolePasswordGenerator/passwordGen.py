6c85ad4f405d58b9299a57427ba18ab69ae3dcf5
Nicolas <53163360+Nicoconte@users.noreply.github.com>
2020-01-08 01:37:17 -0300

    Update passwordGen.py

--- ConsolePasswordGenerator/passwordGen.py
+++ ConsolePasswordGenerator/passwordGen.py
@@ -17,7 +17,7 @@ def password_generator():
             password = ""
             #int i = length; i > 0; i--
             for i in range(length,0,-1):
-                password = password + crypto_generator.choice(values) #concatenate all the choices from values with "p"
+                password = password + crypto_generator.choice(values) #concatenate all the choices from values with "password"
             return password
     except ValueError:
         print("The entry must be a number, let´s try again :) ")
