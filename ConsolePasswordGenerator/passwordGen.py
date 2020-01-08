d465ab9c54f50d7859f4ad88c11d7f86a992e1d3
Nicolas <53163360+Nicoconte@users.noreply.github.com>
2020-01-08 01:18:08 -0300

    Add files via upload
    I made this password generator using System Random class (Python). Enjoy it!

---
+++ ConsolePasswordGenerator/passwordGen.py
@@ -0,0 +1,25 @@
+from random import SystemRandom
+import time
+
+def password_generator():
+    try:
+        length = int(input("> "))
+        if length <= 15 or length >= 33: #valid the length (it will be used by AES method)
+            print("Invalid length, try again")
+            return password_generator()
+        else:
+            print("Please, wait a second")
+            time.sleep(0.5) #this make it look cooler(?
+
+            values = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+            crypto_generator = SystemRandom() #Call the method from SystemRandom class
+            p = ""
+            #int i = length; i > 0; i--
+            for i in range(length,0,-1):
+                p = p + crypto_generator.choice(values) #concatenate all the choices from values with "p"
+            return p
+    except ValueError:
+        print("The entry must be a number, let´s try again :) ")
+
+print("Password generated ", password_generator())
